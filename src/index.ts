b8102aaff6f88abd5b7eba9178688a09d7fc5636
cumul <gg6123@naver.com>
2023-08-10 00:34:46 +0900

    feat: export CLIError from entry point

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { CLI } from './cli';
 export type { Command, CommandArgs, CommandResult, CommandStatic } from './command';
 export { options } from './command';
+export { CLIError } from './error';
 export { Args, Argv, Stderr, Stdin, Stdout } from './id';
 export { array } from './util';
